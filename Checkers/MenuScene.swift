b9a2cb269839553ab4da5d59fdaa3dfaa69015ab
Toma Alexandru <bartimaeus1073@gmail.com>
2016-12-21 19:11:38 +0200

    Created menu scene

---
+++ Checkers/MenuScene.swift
@@ -0,0 +1,16 @@
+//
+//  MenuScene.swift
+//  Checkers
+//
+//  Created by Toma Alexandru on 21/12/2016.
+//  Copyright © 2016 bart games. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class MenuScene: SKScene {
+    override func didMove(to view: SKView) {
+        print("Here");
+    }
+}
